e09ed0b250a90cdadfa77b49f014539c384216df
cos <72692386+junzerokim@users.noreply.github.com>
2021-09-01 23:30:10 +0900

    study

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('hello world')
+const movies = [
+  {
+    title: 'Amadeus',
+    score: 99,
+    year: 1984
+  },
+  {
+    title:  'Sharknado',
+    score: 35,
+    year: 2013
+  },
+  {
+    title: '13 Going On 30',
+    score: 70,
+    year: 2004
+  },
+  {
+    title: 'Stand By Me',
+    score: 85,
+    year: 1986
+  },
+  {
+    title: 'Waterworld',
+    score: 62,
+    year: 1995
+  },
+  {
+    title: 'Jingle All The Way',
+    score: 71,
+    year: 1996
+  },
+  {
+    title: 'Parasite',
+    score: 95,
+    year: 2019
+  },
+  {
+    title: 'Nothing Hill',
+    score: 77,
+    year: 1999
+  },
+  {
+    title: 'Alien',
+    score: 90,
+    year: 1979
+  }
+]
+
+const highestRated = movies.reduce((bestMovie, currMovie) => {
+  if(currMovie.score < bestMovie.score) {
+    return currMovie;
+  }
+  return bestMovie;
+})
